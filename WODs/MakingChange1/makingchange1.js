3b833fc27a89e4ecc07766f331d6230243eadd12
kwtkst <89583746+kwtkst@users.noreply.github.com>
2022-03-08 21:53:11 +0000

    wods

---
+++ WODs/MakingChange1/makingchange1.js
@@ -0,0 +1,20 @@
+var dollarvalue = 1.75;
+var amount = dollarvalue*100;
+
+
+//quarters
+var numofquarters = parseInt(amount/25);
+var quartersremaining = amount%25;
+
+//dimes
+var numofdimes = parseInt(quartersremaining/10);
+var dimesremaining = quartersremaining%10;
+
+//nickels
+var numofnickels = parseInt(dimesremaining/5)
+var nickelsremaining = dimesremaining%5;
+
+//pennies
+var numofpennies = nickelsremaining;
+
+console.log(`We need ${numofquarters} quarters, ${numofdimes} dimes, ${numofnickels} and ${numofpennies} pennies to make $${dollarvalue}`)
