b724af1876da38cb71a3a5d12d373d0d168cb382
kwtkst <89583746+kwtkst@users.noreply.github.com>
2022-03-09 18:17:03 +0000

    wfd

---
+++ Lab10/Ex1.js
@@ -0,0 +1,16 @@
+day = 26;
+month = "August"
+year = 2000;
+
+step1=year - 2000;
+step2=parseInt(step1/4);
+step3 = step2+step1
+step4 = 1;
+step6 = step4+step3
+step7 = day + step6;
+step8 = step7;
+step9 = step8%7
+
+
+console.log(step9);
+
