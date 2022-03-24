a57a18dd3c9d8b5f84231be05bcf84903e63d707
kwtkst <89583746+kwtkst@users.noreply.github.com>
2022-03-24 20:42:00 +0100

    1

---
+++ Brake_Katherine_Assignment1/public/products_data.js
@@ -0,0 +1,27 @@
+var product1  = {
+    brand : "Minolta Weathermatic",
+      price : 65.00 ,
+      image : "/Users/katherinebrake/ITM352_S22_repo/Brake_Katherine_Assignment1/Images/minolta.jpeg"
+};
+var product2 = {
+    brand : "Olympus Zoom 170",
+    price : 220.00 ,
+    image : "/Users/katherinebrake/ITM352_S22_repo/Brake_Katherine_Assignment1/Images/olympus170.jpeg" 
+};
+var product3 = {
+    brand : "Yashica Fx-103",
+      price : 105.00 ,
+      image : "/Users/katherinebrake/ITM352_S22_repo/Brake_Katherine_Assignment1/Images/yashica.jpeg" 
+};
+var product4 = {
+    brand : "Fujifilm DL-270",
+      price : 55.00 ,
+      image : "/Users/katherinebrake/ITM352_S22_repo/Brake_Katherine_Assignment1/Images/discovery270.jpeg" 
+};
+var product5 = {
+    brand : "Canon AE-1 Program",
+    price : 280.00 ,
+    image : "/Users/katherinebrake/ITM352_S22_repo/Brake_Katherine_Assignment1/Images/canon.jpeg"
+};
+
+var products = [product1,product2,product3,product4,product5]
