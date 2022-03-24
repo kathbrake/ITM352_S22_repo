af5d2deaee37ee04786ff86418fa08e28575d7a8
kwtkst <89583746+kwtkst@users.noreply.github.com>
2022-03-24 23:35:58 +0100

    !

--- Brake_Katherine_Assignment1/public/products_data.js
+++ Brake_Katherine_Assignment1/public/products_data.js
@@ -1,27 +1,27 @@
 var product1  = {
     brand : "Minolta Weathermatic",
       price : 65.00 ,
-      image : "/Users/katherinebrake/Documents/GitHub/ITM352_S22_repo 8.20.09 PM/Brake_Katherine_Assignment1/Image/discovery270.jpeg"
+      image : "https://github.com/kathbrake/ITM352_S22_repo/blob/main/Brake_Katherine_Assignment1/Image/minolta.jpeg?raw=true"
 };
 var product2 = {
     brand : "Olympus Zoom 170",
     price : 220.00 ,
-    image : "/Users/katherinebrake/ITM352_S22_repo/Brake_Katherine_Assignment1/Images/olympus170.jpeg" 
+    image : "https://github.com/kathbrake/ITM352_S22_repo/blob/main/Brake_Katherine_Assignment1/Image/olympus170.jpeg?raw=true" 
 };
 var product3 = {
     brand : "Yashica Fx-103",
       price : 105.00 ,
-      image : "/Users/katherinebrake/ITM352_S22_repo/Brake_Katherine_Assignment1/Images/yashica.jpeg" 
+      image : "https://github.com/kathbrake/ITM352_S22_repo/blob/main/Brake_Katherine_Assignment1/Image/yashica.jpeg?raw=true" 
 };
 var product4 = {
     brand : "Fujifilm DL-270",
       price : 55.00 ,
-      image : "/Users/katherinebrake/ITM352_S22_repo/Brake_Katherine_Assignment1/Images/discovery270.jpeg" 
+      image : "https://github.com/kathbrake/ITM352_S22_repo/blob/main/Brake_Katherine_Assignment1/Image/discovery270.jpeg?raw=true" 
 };
 var product5 = {
     brand : "Canon AE-1 Program",
     price : 280.00 ,
-    image : ""
+    image : "https://github.com/kathbrake/ITM352_S22_repo/blob/main/Brake_Katherine_Assignment1/Image/canon.jpeg?raw=true"
 };
 
 var products = [product1,product2,product3,product4,product5]
